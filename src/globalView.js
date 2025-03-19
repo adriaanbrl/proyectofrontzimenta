3c6406d3ad8fd8daaa3a42ed24098c806e6b8e8b
Luis <luisjimenezfernandez11@gmail.com>
2025-03-19 15:55:24 +0100

    ViewGlobal ya Creado

---
+++ src/globalView.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function GlobalView() {
+  return (
+    <div className="container">
+      <header className="text-center mt-5">
+        <img src="/img/logo_zimenta.png" alt="Logo" className="img-fluid" />
+      </header>
+
+      <div id="carouselExample" className="carousel slide mt-5" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="/img/img1.jpg" className="d-block w-100" alt="First slide" />
+          </div>
+          <div className="carousel-item">
+            <img src="/img/img2.jpg" className="d-block w-100" alt="Second slide" />
+          </div>
+          <div className="carousel-item">
+            <img src="/img/img3.jpg" className="d-block w-100" alt="Third slide" />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <section className="text-center mt-5">
+        <p>Descripción de la página</p>
+        <button className="btn btn-primary">Eres cliente</button>
+      </section>
+    </div>
+  );
+}
+
+export default GlobalView;

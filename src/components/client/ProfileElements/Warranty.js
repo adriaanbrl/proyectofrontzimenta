66682e66cb0351967dcb23cc166dfd5b3dfcfd65
Luis <luisjimenezfernandez11@gmail.com>
2025-05-14 16:21:58 +0200

    Cambio diseño en las Incidencias

--- src/components/client/ProfileElements/Warranty.js
+++ src/components/client/ProfileElements/Warranty.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form, Alert, Row, Col } from 'react-bootstrap';
+import { Button, Form, Alert, Row, Col, Container, Card } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ChevronLeft, File } from 'lucide-react';
 
@@ -16,7 +16,7 @@ function Warranty() {
   const [error, setError] = useState('');
   const [buildingId, setBuildingId] = useState(null);
   const [loadingBuildingId, setLoadingBuildingId] = useState(true);
-  const [imagen, setImagen] = useState(null); // Nuevo estado para la imagen
+  const [imagen, setImagen] = useState(null);
 
   useEffect(() => {
     document.title = "Reportar Incidencia de Garantía";
@@ -95,7 +95,7 @@ function Warranty() {
         `http://localhost:8080/api/buildings/${buildingId}/incidents?categoryId=${parseInt(categoriaId, 10)}&roomId=${parseInt(estanciaId, 10)}`,
         {
           method: 'POST',
-          body: formData, // Usar FormData para enviar archivos
+          body: formData,
         }
       );
 
@@ -106,7 +106,7 @@ function Warranty() {
         setDescripcion('');
         setCategoriaId('');
         setEstanciaId('');
-        setImagen(null); // Limpiar el estado de la imagen
+        setImagen(null);
       } else {
         const errorText = await response.text();
         console.error('Error al registrar la incidencia:', response.status, errorText);
@@ -121,7 +121,7 @@ function Warranty() {
   };
 
   if (loadingBuildingId) {
-    return <div>Cargando información del edificio...</div>;
+    return <div className="d-flex justify-content-center align-items-center vh-100">Cargando información del edificio...</div>;
   }
 
   const handleGoBack = () => {
@@ -129,90 +129,111 @@ function Warranty() {
   };
 
   return (
-    <div className="p-4 rounded-lg shadow-md bg-light">
-      <div className="d-flex justify-content-center align-items-center mb-4">
-        <Button
-          variant="link"
-          onClick={handleGoBack}
-          className="back-button me-3"
-          aria-label="Volver atrás"
-          style={{ padding: 0 }}
-        >
-          <ChevronLeft size={20} color="orange" />
-        </Button>
-        <h1 className="text-center mb-0">Reportar Incidencia</h1>
-      </div>
-      {error && <Alert variant="danger">{error}</Alert>}
-      <Form onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="titulo">
-            <Form.Label>Título de la Incidencia:</Form.Label>
-            <Form.Control
-              type="text"
-              value={titulo}
-              onChange={(e) => setTitulo(e.target.value)}
-              required
-              placeholder="Ej: Fuga en tubería del baño"
-            />
-          </Form.Group>
-        </Row>
+    <Container className="py-5">
+      <Row className="justify-content-center">
+        <Col md={8} lg={6}>
+          <Card className="shadow-sm rounded-lg border-0">
+            <Card.Body className="p-2">
+               <div className="d-flex justify-content-center align-items-center mb-3">
+                          <Button
+                            variant="link"
+                            onClick={handleGoBack}
+                            className="back-button me-3"
+                            aria-label="Volver atrás"
+                            style={{ padding: 0 }}
+                          >
+                            <ChevronLeft size={20} color="orange" />
+                          </Button>
+                          <h2 className="historic-title text-center mb-0">Reportar Incidencias</h2>
+                        </div>
+              {error && <Alert variant="danger" className="mb-3">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="titulo">
+                  <Form.Label className="fw-semibold">Título de la Incidencia:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={titulo}
+                    onChange={(e) => setTitulo(e.target.value)}
+                    required
+                    placeholder="Ej: Fuga en tubería del baño"
+                    className="rounded-pill border-0 shadow-sm"
+                  />
+                </Form.Group>
 
-        <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="categoriaId">
-            <Form.Label>Categoría de la Incidencia:</Form.Label>
-            <Form.Control
-              as="select"
-              value={categoriaId}
-              onChange={(e) => setCategoriaId(e.target.value)}
-            >
-              <option value="">Seleccionar categoría</option>
-              {categorias.map((cat) => (
-                <option key={cat.id} value={cat.id}>{cat.name}</option>
-              ))}
-            </Form.Control>
-          </Form.Group>
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="6" controlId="categoriaId" className='mt-2'>
+                    <Form.Label className="fw-semibold">Categoría de la Incidencia:</Form.Label>
+                    <Form.Control
+                      as="select"
+                      value={categoriaId}
+                      onChange={(e) => setCategoriaId(e.target.value)}
+                      className="rounded-pill border-0 shadow-sm"
+                    >
+                      <option value="">Seleccionar categoría</option>
+                      {categorias.map((cat) => (
+                        <option key={cat.id} value={cat.id}>{cat.name}</option>
+                      ))}
+                    </Form.Control>
+                  </Form.Group>
 
-          <Form.Group as={Col} md="6" controlId="estanciaId">
-            <Form.Label>Estancia:</Form.Label>
-            <Form.Control
-              as="select"
-              value={estanciaId}
-              onChange={(e) => setEstanciaId(e.target.value)}
-            >
-              <option value="">Seleccionar estancia</option>
-              {estancias.map((estancia) => (
-                <option key={estancia.id} value={estancia.id}>{estancia.name}</option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-        </Row>
+                  <Form.Group as={Col} md="6" controlId="estanciaId" className='mt-2'>
+                    <Form.Label className="fw-semibold">Estancia:</Form.Label>
+                    <Form.Control
+                      as="select"
+                      value={estanciaId}
+                      onChange={(e) => setEstanciaId(e.target.value)}
+                      className="rounded-pill border-0 shadow-sm"
+                    >
+                      <option value="">Seleccionar estancia</option>
+                      {estancias.map((estancia) => (
+                        <option key={estancia.id} value={estancia.id}>{estancia.name}</option>
+                      ))}
+                    </Form.Control>
+                  </Form.Group>
+                </Row>
 
-        <Form.Group className="mb-3" controlId="descripcion">
-          <Form.Label>Descripción Detallada:</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={5}
-            value={descripcion}
-            onChange={(e) => setDescripcion(e.target.value)}
-            required
-            placeholder="Describa detalladamente el problema..."
-          />
-        </Form.Group>
+                <Form.Group className="mb-3" controlId="descripcion">
+                  <Form.Label className="fw-semibold mt-2">Descripción Detallada:</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={5}
+                    value={descripcion}
+                    onChange={(e) => setDescripcion(e.target.value)}
+                    required
+                    placeholder="Describa detalladamente el problema..."
+                    className="rounded-lg border-0 shadow-sm"
+                  />
+                </Form.Group>
 
-        {/* Nuevo campo para la imagen */}
-        <Form.Group className="mb-3" controlId="imagen">
-          <Form.Label>Imagen de la Incidencia (opcional):</Form.Label>
-          <Form.Control
-            type="file"
-            onChange={handleImagenChange}
-          />
-        </Form.Group>
+                <Form.Group className="mb-3" controlId="imagen">
+                  <Form.Label className="fw-semibold">Imagen de la Incidencia (opcional):</Form.Label>
+                  <Form.Control
+                    type="file"
+                    onChange={handleImagenChange}
+                    className="rounded-pill border-0 shadow-sm"
+                  />
+                  {imagen && (
+                    <div className="mt-2 text-muted small">
+                      <File size={16} className="me-1" />
+                      {imagen.name}
+                    </div>
+                  )}
+                </Form.Group>
 
-        <Button variant="primary" type="submit" disabled={isSubmitting} className="w-100">
-          {isSubmitting ? 'Enviando...' : 'Enviar Reporte'}
-        </Button>
-      </Form>
-    </div>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="w-100 rounded-pill fw-bold shadow"
+                >
+                  {isSubmitting ? 'Enviando...' : 'Enviar Reporte'}
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
